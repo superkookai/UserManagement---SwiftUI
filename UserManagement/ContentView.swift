ed6fa1a687547a43ce23f1d1eddcfad004369243
Weerawut Chaiyasomboon <weerawut@live.com>
2024-01-12 10:10:25 +0700

    Initial Commit

---
+++ UserManagement/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserManagement
+//
+//  Created by Weerawut Chaiyasomboon on 12/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserManagement/UserManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserManagementApp.swift
+//  UserManagement
+//
+//  Created by Weerawut Chaiyasomboon on 12/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct UserManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
